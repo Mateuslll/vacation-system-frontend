8dc5f4fffc20fe71a4304cc4b6b5007f002100dc
mateuslll <lima.mt96@gmail.com>
2025-10-07 22:02:36 +0100

    feat: configuração de middleware

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('Token');
+
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    if (!token) {
+      return NextResponse.redirect(new URL('/sign-in', request.url));
+    }
+  }
+
+  if (request.nextUrl.pathname.startsWith('/sign-in')) {
+    if (token) {
+      return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
+  }
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*', '/sign-in']
+};
